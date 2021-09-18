89d49489badd8d71b60dce28767fd08c8c2fc165
franciscocordasneves <90935947+franciscocordasneves@users.noreply.github.com>
2021-09-18 18:54:30 -0300

    Area do triângolo.c
    TIC aria do triangulo exe.1

---
+++ Area do triângolo.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+
+int main(){
+      float lado1 , lado2;
+    float Area ;
+    
+    printf("Digite os dois lado do Quadrad ");
+    
+	scanf("%f%f",&lado1,&lado2);
+    
+	Area = lado1 * lado2;
+    
+	 printf(" A area do triângulo é = %f", Area);
+
+     return 0 ; 
+}
